296801b7f3ba54e0f8708d9e4f35985705a918f7
samandarbotiraliyev6-star <samandarbotiraliyev6@gmail.com>
2026-05-31 14:38:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
+TOKEN = "TOKENNI_KEYIN_QOYAMIZ"
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot ishlayapti!")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, handle_message))
+
+app.run_polling()
