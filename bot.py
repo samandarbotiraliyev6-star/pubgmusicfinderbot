1dab70f4acb30d7726d252bc8880281fc3f3fe91
samandarbotiraliyev6-star <samandarbotiraliyev6@gmail.com>
2026-05-31 15:53:28 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters
 TOKEN = "8939218526:AAGsNc08cgzq1hUrQvw4oTXlwYuBj-1zjp4"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot ishlayapti!")
+    await update.message.reply_text("shu kanallarga obuna boling https://www.youtube.com/@Vampires_play!")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, handle_message))
