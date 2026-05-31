b984de01f7e4cc27f4471af0dc44e8327f35e563
samandarbotiraliyev6-star <samandarbotiraliyev6@gmail.com>
2026-05-31 15:31:30 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-TOKEN = "8939218526: AAGsNc08cgzq1hUrQvw4oTXlwYuBj-1zjp4"
+TOKEN = "8939218526:AAGsNc08cgzq1hUrQvw4oTXlwYuBj-1zjp4"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot ishlayapti!")
